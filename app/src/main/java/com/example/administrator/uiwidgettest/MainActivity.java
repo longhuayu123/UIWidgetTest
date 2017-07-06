c19973deec7a298b45fb931d7d254a1bfc2f39d4
lhy <lhy1234567891011@163.com>
2017-07-06 20:57:46 +0800

    111

--- app/src/main/java/com/example/administrator/uiwidgettest/MainActivity.java
+++ app/src/main/java/com/example/administrator/uiwidgettest/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.administrator.uiwidgettest;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText editText;
+    private ImageView imageView;
     private Button button1;
     private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
     private ProgressBar progressBar;
 
     @Override
@@ -26,11 +34,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void init() {
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
         progressBar = (ProgressBar) findViewById(R.id.progress);
         editText = (EditText) findViewById(R.id.edit_text);
-        button2.setOnClickListener(this);
-        button1.setOnClickListener(this);
+        imageView = (ImageView) findViewById(R.id.image_1);
 
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
     }
 
     @Override
@@ -41,6 +56,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_LONG).show();
                 break;
             case R.id.button2:
+                imageView.setImageResource(R.drawable.t017594895117439388);
+                break;
+            case R.id.button3:
                 /*if (progressBar.getVisibility() == View.GONE)
                     progressBar.setVisibility(View.VISIBLE);
                 else {
@@ -49,6 +67,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 int progress = progressBar.getProgress();
                 progress = progress + 10;
                 progressBar.setProgress(progress);
+                break;
+            case R.id.image_1:
+                Toast.makeText(MainActivity.this, "这是图片", Toast.LENGTH_LONG);
+                break;
+            case R.id.button4:
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("this is dialog");
+                dialog.setMessage("something is important");
+                dialog.setCancelable(false);//不可撤销 的
+                dialog.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                dialog.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                dialog.show();
+                break;
+            case R.id.button5:
+                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setTitle("this is progressDialog");
+                progressDialog.setMessage("Loading...");
+                progressDialog.setCancelable(true);//可撤销的
+                progressDialog.show();
+                //progressDialog.dismiss();
                 break;
             default:
                 break;
